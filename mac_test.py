ed400e1201af8b2966e8eed2d5bab767c46fd2ca
Ian Bicket <bicket@bicket-macbookpro.roam.corp.google.com>
2019-02-19 09:41:43 -0800

    updates to mac_test file

--- mac_test.py
+++ mac_test.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python3
+import word_embedder
 
 ### a test file for word_embedder.py made for mac
 DATAPATH = 'data'
@@ -13,7 +14,21 @@ mk = 'test_mod'
 intest = 'paradise_lost.txt'
 # get text
 with open(infile) as ifdat:
-    text = ifdata.read()
+    text = ifdat.read()
+
+with open('data/paradise_lost.txt.rtf_n_4_table_compress.json') as dfile:
+	w2vs = json.loads(dfile.read())
+    # so json can't read integer keys. So, change it
+    for w in w2vs:
+    	print(w)
+        wval = w2vs[w]
+        buff = {}
+        for m in wval: 
+        	if m.isnumeric():
+            	buff[int(m)] = wval[m]
+            else:
+            	buff[m] = wval[m]
+                w2vs[w] = buff
 
 
 
